9cb5ba846223955455fcf261576ace3ab334b692
Adriana Chipana <adrijonas16@gmail.com>
2026-01-20 09:50:29 -0500

    borrar corigo comentado

--- src/paginas/CreateOpportunity/CreateOpportunity.tsx
+++ src/paginas/CreateOpportunity/CreateOpportunity.tsx
@@ -13,7 +13,6 @@ import {
 import { useNavigate, useLocation } from "react-router-dom";
 import { CloseOutlined, CalendarOutlined } from "@ant-design/icons";
 import moment, { type Moment } from "moment";
-// @ts-ignore
 import "moment/locale/es";
 import {
   insertarOportunidadHistorialRegistrado,
